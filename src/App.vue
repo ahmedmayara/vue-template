fea5d8655b13ffbb25322a38e3f99c60188cc4c3
Ahmed <ahmedmayara789@gmail.com>
2023-09-09 12:42:30 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router/auto";
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="grid place-items-center h-screen">
+    <h1 class="text-6xl font-bold">Vue 3 + Vite + TypeScript + Tailwind CSS</h1>
+  </main>
+</template>
