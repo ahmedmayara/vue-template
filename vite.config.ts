3306c29da2b52029a825a84148fbd929b22fb455
Ahmed <ahmedmayara789@gmail.com>
2023-09-15 17:02:13 +0100

    feat: add unplugin-icons to the project

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import VueRouter from "unplugin-vue-router/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueJsx()],
+  plugins: [VueRouter(), vue(), vueJsx(), Icons({ compiler: "vue3" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
