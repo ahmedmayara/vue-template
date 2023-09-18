cf62f2dd9c56534c1b437454596e4fae93b15420
Ahmed <ahmedmayara789@gmail.com>
2023-09-18 10:57:09 +0100

    chore: add husky

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="grid place-items-center h-screen">
+  <main class="grid h-screen place-items-center">
     <h1 class="text-6xl font-bold">Vue 3 + Vite + TypeScript + Tailwind CSS</h1>
   </main>
 </template>
